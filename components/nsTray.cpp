a292ad87bc1e0c90bd0d8d9feb6d7018e062b879
Kid.Duo <Kid.Duo@1aa13f37-862e-0410-bb36-2ba89cdadaa9>
2007-04-24 08:47:01 +0000

    FIX: fix overlay in a correct position.

--- components/nsTray.cpp
+++ components/nsTray.cpp
@@ -20,10 +20,14 @@ nsTray::~nsTray()
 /* void showTray (); */
 NS_IMETHODIMP nsTray::ShowTray()
 {
-    systray_icon = gtk_status_icon_new();
-    icon = gdk_pixbuf_new_from_xpm_data((const char**)tray_icon);
-    gtk_status_icon_set_from_pixbuf(GTK_STATUS_ICON(systray_icon), GDK_PIXBUF(icon));
-    gtk_status_icon_set_tooltip(systray_icon, "Firetray");
+    if (!systray_icon) {
+        systray_icon = gtk_status_icon_new();
+        icon = gdk_pixbuf_new_from_xpm_data((const char**)tray_icon);
+        gtk_status_icon_set_from_pixbuf(GTK_STATUS_ICON(systray_icon), GDK_PIXBUF(icon));
+        gtk_status_icon_set_tooltip(systray_icon, "Firetray");
+    } else {
+        gtk_status_icon_set_visible(systray_icon, TRUE);
+    }
 
 	return NS_OK;
 }
@@ -31,5 +35,7 @@ NS_IMETHODIMP nsTray::ShowTray()
 /* void hideTray (); */
 NS_IMETHODIMP nsTray::HideTray()
 {
-    return NS_ERROR_NOT_IMPLEMENTED;
+    gtk_status_icon_set_visible(systray_icon, FALSE);
+
+	return NS_OK;
 }
