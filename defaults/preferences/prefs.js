16b5b75982e18409c4ef0887d25fcd4b43d97d82
fsoltrash <fsoltrash@1aa13f37-862e-0410-bb36-2ba89cdadaa9>
2008-02-27 18:11:37 +0000

    - added fr-FR localization - added default preferences

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,1 +1,5 @@
 pref("extensions.{9533f794-00b4-4354-aa15-c2bbda6989f8}.description","chrome://firetray/locale/core.properties");
+
+pref("extensions.firetray.close_to_tray", false);
+pref("extensions.firetray.start_minimized", false);
+pref("extensions.firetray.confirm_exit", true);
