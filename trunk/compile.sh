517f8f54f3f1b1411eef1c252fb8d5df7eedc094
fsoltrash <fsoltrash@1aa13f37-862e-0410-bb36-2ba89cdadaa9>
2009-06-18 13:44:31 +0000

    Added some new translations

--- trunk/compile.sh
+++ trunk/compile.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-#export GECKO_SDK=/Temp/FireTray_Dev/xulrunner-sdk/sdk/ 
-export GECKO_SDK=/usr/lib/xulrunner-devel-1.9.0.1/
-export GECKO_SDK_BIN=/usr/lib/xulrunner-devel-1.9.0.1/bin/
-export GECKO_SDK_INCLUDE=/usr/include/xulrunner-1.9.0.1/unstable/ 
-export GECKO_SDK_IDL=/usr/share/idl/xulrunner-1.9.0.1/unstable
-export GECKO_SDK_LIB=/usr/lib/xulrunner-devel-1.9.0.1/sdk/lib
+export GECKO_SDK=/usr/lib/xulrunner-devel-1.9/
+export GECKO_SDK_BIN=/usr/lib/xulrunner-devel-1.9/bin/
+export GECKO_SDK_INCLUDE=/usr/include/xulrunner-1.9/unstable/ 
+export GECKO_SDK_IDL=/usr/share/idl/xulrunner-1.9/unstable
+export GECKO_SDK_LIB=/usr/lib/xulrunner-devel-1.9/sdk/lib
 
 FILE=firetray.xpi
 
@@ -13,6 +12,7 @@ rm $FILE
 scons $FILE
 if [ -e $FILE ] 
 then
+  zip firetray.xpi components/libnptray_i386.so
   echo ok
 else 
   echo error compiling $FILE
