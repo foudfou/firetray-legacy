5fbdeb8dc5fea774bfa8c5e0f16597476a272802
fsoltrash <fsoltrash@1aa13f37-862e-0410-bb36-2ba89cdadaa9>
2009-10-05 16:33:49 +0000

    FIXED Segfaults in 64 bit systems

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export GECKO_SDK=/usr/lib/xulrunner-devel-1.9/
-export GECKO_SDK_BIN=/usr/lib/xulrunner-devel-1.9/bin/
-export GECKO_SDK_INCLUDE=/usr/include/xulrunner-1.9/unstable/ 
-export GECKO_SDK_IDL=/usr/share/idl/xulrunner-1.9/unstable
-export GECKO_SDK_LIB=/usr/lib/xulrunner-devel-1.9/sdk/lib
+export GECKO_SDK=/usr/lib/xulrunner-devel-1.9.1/
+export GECKO_SDK_BIN=/usr/lib/xulrunner-devel-1.9.1/bin/
+export GECKO_SDK_INCLUDE=/usr/lib/xulrunner-devel-1.9.1/include/
+export GECKO_SDK_IDL=/usr/lib/xulrunner-devel-1.9.1/idl/
+export GECKO_SDK_LIB=/usr/lib/xulrunner-devel-1.9.1/lib/
 
 FILE=firetray.xpi
 
@@ -12,7 +12,7 @@ rm $FILE
 scons $FILE
 if [ -e $FILE ] 
 then
-  zip $FILE components/libnptray_i386.so
+
   echo ok
 else 
   echo error compiling $FILE
